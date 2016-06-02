b27441c44315178c964ea88adafe832cee846b51
Ulf Möllerström <ulf@scoreit.secls>
2016-06-02 16:32:33 +0200

    Slut för i dag-check in

--- Umbraco With LocalDb/App_Code/EmploymentDateExtensions.cs
+++ Umbraco With LocalDb/App_Code/EmploymentDateExtensions.cs
@@ -4,15 +4,24 @@ using Umbraco.Core.Models;
 
 namespace Umbraco_With_LocalDb.App_Code
 {
-    public static class EmploymentDateExtentions
+    /// <summary>
+    /// Extensions for an Umbraco-member that has an EmploymentDate-property of type DateTime
+    /// </summary>
+    public static class EmploymentDateExtensions
     {
+
+        /// <summary>
+        /// Gets the employment date for an Umbraco-membertype that has an EmploymentDate-property
+        /// </summary>
+        /// <param name="member">the member</param>
+        /// <returns></returns>
         public static DateTime GetEmploymentDate(this IMember member)
         {
             var noDate = DateTime.Today;
             var property = GetEmploymentDateProperty(member);
 
             if (property == null) return noDate;
-
+                
             var employmentDate = property.Value as DateTime?;
             return employmentDate ?? noDate;
         }
@@ -23,7 +32,7 @@ namespace Umbraco_With_LocalDb.App_Code
             if (property == null) return;
             property.Value = employmentDate;
         }
-
+          
         private static Property GetEmploymentDateProperty(IMember member)
         {
             return member.Properties.FirstOrDefault(p => p.Alias.Equals("EmploymentDate", StringComparison.InvariantCultureIgnoreCase));
